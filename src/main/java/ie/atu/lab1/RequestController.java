4b68cafce7a56687846651198a12adf5700136ee
Paddy downey <G00392495@atu.ie>
2023-09-27 15:41:19 +0100

    created the "details" method

--- src/main/java/ie/atu/lab1/RequestController.java
+++ src/main/java/ie/atu/lab1/RequestController.java
@@ -2,6 +2,7 @@ package ie.atu.lab1;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,4 +16,8 @@ public class RequestController {
         public String greetByName(@PathVariable String name){
         return "hello" + name;
     }
+
+    @GetMapping("/details")
+    public String Details(@RequestParam String Name, @RequestParam int age)
+    {return "name: " + Name + " age: " + age;}
 }
