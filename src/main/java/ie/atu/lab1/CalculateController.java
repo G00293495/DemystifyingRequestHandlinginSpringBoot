0c7de2e229ea2a42f1e4890f02b33545f8b63061
Paddy downey <G00392495@atu.ie>
2023-10-04 13:36:04 +0100

    created the "calculate" part i method

--- src/main/java/ie/atu/lab1/CalculateController.java
+++ src/main/java/ie/atu/lab1/CalculateController.java
@@ -8,5 +8,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class CalculateController {
 
     @GetMapping("/calculate")
-    public String Calculate(@RequestParam int num1, @RequestParam int num2){}
+    public int Calculate(@RequestParam int num1, @RequestParam int num2, @RequestParam String operation) {
+       switch (operation) {
+           case ("add"):
+               return num1 + num2;
+
+           case ("divide"):
+               return num1 / num2;
+
+           case ("subtract"):
+               return num1 - num2;
+
+           case ("multiply"):
+               return num1 * num2;
+       }
+        return 0;
+    }
 }
